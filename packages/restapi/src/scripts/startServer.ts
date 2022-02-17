b635e223e3d36e0dbc76f206623391ecbb68018d
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-17 16:34:07 +0100

    refactor(restapi): add startServer
    Co-authored-by: Adrián Martínez Jiménez <adrianmjim199434@gmail.com>

---
+++ packages/restapi/src/scripts/startServer.ts
@@ -0,0 +1,31 @@
+import http from 'http';
+
+import { NestApplicationOptions } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyServerOptions } from 'fastify';
+
+import { AppConfig } from '../app/domain/config/AppConfig';
+import { AppModule } from '../app/integration/nest/module/AppModule';
+
+async function startServer(): Promise<void> {
+  const fastifyServerOptions: FastifyServerOptions<http.Server> = {
+    logger: true,
+  };
+  const fastifyAdapter: FastifyAdapter = new FastifyAdapter(fastifyServerOptions);
+
+  const nestApplicationOptions: NestApplicationOptions = {
+    cors: { origin: '*' },
+  };
+  const nestApplication: NestFastifyApplication = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastifyAdapter,
+    nestApplicationOptions,
+  );
+
+  const appConfig: AppConfig = nestApplication.get(AppConfig);
+
+  await nestApplication.listen(appConfig.port, appConfig.host);
+}
+
+void startServer();
