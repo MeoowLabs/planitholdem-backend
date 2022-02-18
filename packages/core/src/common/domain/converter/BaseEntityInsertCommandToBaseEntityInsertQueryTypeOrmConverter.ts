826353081d5c96d9a65b52c840120223b5bb4bec
Adrián Martínez Jiménez <adrianmjim199434@gmail.com>
2022-02-19 00:34:37 +0100

    Add BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverter

---
+++ packages/core/src/common/domain/converter/BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverter.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { BaseEntityTypeOrm } from '../../integration/typeOrm/model/BaseEntityTypeOrm';
+import { BaseEntityInsertCommand } from '../command/BaseEntityInsertCommand';
+import { Converter } from './Converter';
+
+@Injectable()
+export class BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverter<
+  TInput extends BaseEntityInsertCommand,
+  TOutput extends DeepPartial<BaseEntityTypeOrm>,
+> implements Converter<TInput, TOutput>
+{
+  public convert(): TOutput {
+    const date: Date = new Date();
+
+    const output: DeepPartial<BaseEntityTypeOrm> = {
+      createdAt: date,
+      id: uuid(),
+      updatedAt: date,
+    };
+
+    return output as TOutput;
+  }
+}
