d3806c681c3a089a382af610b62ae1e923c6373b
Adrián Martínez Jiménez <adrianmjim199434@gmail.com>
2022-02-18 16:25:20 +0100

    Add UserInsertCommand

---
+++ packages/core/src/user/domain/command/UserInsertCommand.ts
@@ -0,0 +1,11 @@
+import { ICommand } from '@nestjs/cqrs';
+
+export class UserInsertCommand implements ICommand {
+  constructor(
+    public readonly name: string,
+    public readonly surname: string,
+    public readonly email: string,
+    public readonly passwordHash: string,
+    public readonly salt: string,
+  ) {}
+}
