8337872e4aee56783c1df8524a423463e273209f
Adrián Martínez Jiménez <adrianmjim199434@gmail.com>
2022-02-19 00:35:54 +0100

    Refactor UserFindQueryToUserFindQueryTypeOrmConverter to extends BaseEntityFindQueryToBaseEntityFindQueryTypeOrmConverter

--- packages/core/src/user/integration/typeOrm/converter/UserFindQueryToUserFindQueryTypeOrmConverter.ts
+++ packages/core/src/user/integration/typeOrm/converter/UserFindQueryToUserFindQueryTypeOrmConverter.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { FindConditions } from 'typeorm';
 
-import { Converter } from '../../../../common/domain/converter/Converter';
+import { BaseEntityFindQueryToBaseEntityFindQueryTypeOrmConverter } from '../../../../common/domain/converter/BaseEntityFindQueryToBaseEntityFindQueryTypeOrmConverter';
 import { UserFindQuery } from '../../../domain/query/UserFindQuery';
 import { UserTypeOrm } from '../model/UserTypeOrm';
 
 @Injectable()
-export class UserFindQueryToUserFindQueryTypeOrmConverter
-  implements Converter<UserFindQuery, FindConditions<UserTypeOrm>>
-{
-  public convert(input: UserFindQuery): FindConditions<UserTypeOrm> {
-    const userFindQueryTypeOrm: FindConditions<UserTypeOrm> = {};
-
-    if (input.id !== undefined) {
-      userFindQueryTypeOrm.id = input.id;
-    }
+export class UserFindQueryToUserFindQueryTypeOrmConverter extends BaseEntityFindQueryToBaseEntityFindQueryTypeOrmConverter<
+  UserFindQuery,
+  FindConditions<UserTypeOrm>
+> {
+  public override convert(input: UserFindQuery): FindConditions<UserTypeOrm> {
+    const userFindQueryTypeOrm: FindConditions<UserTypeOrm> = super.convert(input);
 
     if (input.email !== undefined) {
       userFindQueryTypeOrm.email = input.email;
