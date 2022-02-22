b40d2e262e6f651488f343ce7e2d447bef165259
Adrian <adrian.martinez@8belts.com>
2022-02-22 18:25:48 +0100

    Refactor BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverterAsync
    Co-authored-by: notaphplover <roberto.pintos.lopez@gmail.com>

---
+++ packages/core/src/common/domain/converter/BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverterAsync.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { BaseEntityTypeOrm } from '../../integration/typeOrm/model/BaseEntityTypeOrm';
+import { BaseEntityInsertCommand } from '../command/BaseEntityInsertCommand';
+import { ConverterAsync } from './ConverterAsync';
+
+@Injectable()
+export abstract class BaseEntityInsertCommandToBaseEntityInsertQueryTypeOrmConverterAsync<
+  TInput extends BaseEntityInsertCommand,
+  TOutput extends DeepPartial<BaseEntityTypeOrm>,
+> implements ConverterAsync<TInput, TOutput>
+{
+  public async convert(input: TInput): Promise<TOutput> {
+    const baseEntityFindQueryTypeOrm: DeepPartial<BaseEntityTypeOrm> =
+      await this.convertToBaseEntityInsertQueryTypeOrm();
+
+    const entityInsertQueryTypeOrm: TOutput = await this.convertToEntityInsertQueryTypeOrm(
+      input,
+      baseEntityFindQueryTypeOrm,
+    );
+
+    return entityInsertQueryTypeOrm;
+  }
+
+  private async convertToBaseEntityInsertQueryTypeOrm(): Promise<DeepPartial<BaseEntityTypeOrm>> {
+    const createdAt: Date = new Date();
+    const updatedAt: Date = new Date(createdAt);
+
+    const id: string = uuid();
+    const output: DeepPartial<BaseEntityTypeOrm> = {
+      createdAt,
+      id,
+      updatedAt,
+    };
+
+    return output;
+  }
+
+  protected abstract convertToEntityInsertQueryTypeOrm(
+    input: TInput,
+    baseEntityFindQueryTypeOrm: DeepPartial<BaseEntityTypeOrm>,
+  ): Promise<TOutput>;
+}
