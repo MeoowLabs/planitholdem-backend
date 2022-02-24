e2bb48c4dc63ee141d501ad57a8c1bad1c8f33d0
Adrian <adrian.martinez@8belts.com>
2022-02-24 12:29:50 +0100

    Refactor UserInsertCommand to extends BaseEntityInsertCommand

--- packages/core/src/user/domain/command/UserInsertCommand.ts
+++ packages/core/src/user/domain/command/UserInsertCommand.ts
@@ -1,10 +1,12 @@
-import { ICommand } from '@nestjs/cqrs';
+import { BaseEntityInsertCommand } from '../../../common/domain/command/BaseEntityInsertCommand';
 
-export class UserInsertCommand implements ICommand {
+export class UserInsertCommand extends BaseEntityInsertCommand {
   constructor(
     public readonly name: string,
     public readonly surname: string,
     public readonly email: string,
     public readonly password: string,
-  ) {}
+  ) {
+    super();
+  }
 }
