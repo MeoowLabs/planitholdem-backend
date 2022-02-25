52ab97e0a11c45febf3cb2c64c9b773d10bc3f91
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-25 12:25:27 +0100

    add FindOneUserManager

---
+++ packages/core/src/user/domain/manager/FindOneUserManager.ts
@@ -0,0 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { FindAdapter } from '../../../common/domain/adapter/FindAdapter';
+import { FindOneManager } from '../../../common/domain/service/FindOneManager';
+import { FindUserTypeOrmAdapter } from '../../integration/typeOrm/adapter/FindUserTypeOrmAdapter';
+import { User } from '../model/User';
+import { UserFindQuery } from '../query/UserFindQuery';
+
+@Injectable()
+export class InsertUserManager extends FindOneManager<User, UserFindQuery> {
+  public constructor(@Inject(FindUserTypeOrmAdapter) findAdapter: FindAdapter<User, UserFindQuery>) {
+    super(findAdapter);
+  }
+}
--- packages/core/src/user/domain/manager/InsertUserManager.ts
+++ packages/core/src/user/domain/manager/InsertUserManager.ts
@@ -2,12 +2,13 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { InsertAdapter } from '../../../common/domain/adapter/InsertAdapter';
 import { InsertOneManager } from '../../../common/domain/service/InsertOneManager';
+import { InsertUserTypeOrmAdapter } from '../../integration/typeOrm/adapter/InsertUserTypeOrmAdapter';
 import { UserInsertCommand } from '../command/UserInsertCommand';
 import { User } from '../model/User';
 
 @Injectable()
 export class InsertUserManager extends InsertOneManager<User, UserInsertCommand> {
-  public constructor(@Inject() insertAdapter: InsertAdapter<User, UserInsertCommand>) {
+  public constructor(@Inject(InsertUserTypeOrmAdapter) insertAdapter: InsertAdapter<User, UserInsertCommand>) {
     super(insertAdapter);
   }
 }
